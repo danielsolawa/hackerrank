3e145ea1b0b98a2b7552e04e7047c3ab661e22a0
Daniel Solawa <danielsolawa@gmail.com>
2018-04-27 12:38:43 +0200

    ArrayReplace CodeFights Solution

---
+++ src/com/danielsolawa/codefights/solutions/ArrayReplace.java
@@ -0,0 +1,16 @@
+package com.danielsolawa.codefights.solutions;
+
+public class ArrayReplace {
+
+	
+	int[] arrayReplace(int[] inputArray, int elemToReplace, int substitutionElem) {
+		for(int i = 0; i < inputArray.length; i++) {
+			if(inputArray[i] == elemToReplace) {
+				inputArray[i] = substitutionElem;
+			}
+		}
+		
+		
+		return inputArray;
+	}
+}

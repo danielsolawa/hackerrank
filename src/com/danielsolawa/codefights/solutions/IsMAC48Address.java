3e1c07ab48dfebcf6e3b8581ad1af5cf73f9367d
Daniel Solawa <danielsolawa@gmail.com>
2018-05-03 09:33:36 +0200

    moved code fights solutions

--- src/com/danielsolawa/codefights/solutions/IsMAC48Address.java
+++
@@ -1,8 +0,0 @@
-package com.danielsolawa.codefights.solutions;
-
-public class IsMAC48Address {
-
-	boolean isMAC48Address(String s) {
-		return s.matches("([0-9a-fA-F]{2}[-]){5}([0-9a-fA-F]{2})");
-	}
-}

832475e712690a73c5271a0e5346ca6dc70ca68f
Daniel Solawa <danielsolawa@gmail.com>
2018-05-22 18:46:22 +0200

    JavaRegex

--- src/com/danielsolawa/solutions/PriorityQueueChallenge.java
+++ src/com/danielsolawa/solutions/PriorityQueueChallenge.java
@@ -35,7 +35,7 @@ public class PriorityQueueChallenge {
 
 
 
-class Student implements Comparable<Student>{
+class Student{
     private int id;
     private String name;
     private double cgpa;
@@ -46,22 +46,7 @@ class Student implements Comparable<Student>{
         this.cgpa = cgpa;
     }
     
-    
-	@Override
-	public int compareTo(Student o2) {
-		int cgpaResult = Double.compare(o2.getCgpa(), cgpa);
-		
-		if(cgpaResult != 0)
-			return cgpaResult;
-		
-		int nameResult = name.compareTo(o2.getName());
-		
-		if(nameResult != 0)
-			return nameResult;
-		
-		return Integer.compare(o2.getId(), id);
-
-	}
+ 
     
 	/*
 	 * getters & setters
@@ -90,9 +75,12 @@ class Student implements Comparable<Student>{
 
 
 
-class Priorities implements Comparator<Student>{
+class Priorities{
 	   List<Student> getStudents(List<String> events){
-	        Queue<Student> qu = new PriorityQueue<>();
+		   PriorityQueue<Student> qu = new PriorityQueue<>(
+	        		Comparator.comparing(Student::getCgpa).reversed()
+	        		.thenComparing(Student::getName)
+	        		.thenComparing(Student::getId));
 	        for(String s : events) {
 	        	if(s.contains("ENTER")) {
 	        		String[] arr = s.replaceAll("ENTER", "").trim().split("\\s+");
@@ -101,25 +89,11 @@ class Priorities implements Comparator<Student>{
 	        	}else
 	        		qu.poll();
 	        }
-	        List<Student> list = new ArrayList<>(qu);
-	        list.sort(this);
-	        return list;
+	       
+	        return new ArrayList<>(qu);
 	    }
 
-	@Override
-	public int compare(Student o1, Student o2) {
-		int cgpaResult = Double.compare(o2.getCgpa(), o1.getCgpa());
-		
-		if(cgpaResult != 0)
-			return cgpaResult;
-		
-		int nameResult = o1.getName().compareTo(o2.getName());
-		
-		if(nameResult != 0)
-			return nameResult;
-		
-		return Integer.compare(o2.getId(), o1.getId());
-	}
+	
 
 
 }
